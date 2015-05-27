64e7a945e727e252705f9a85a209087dc33c5bbe
Ana Moura <anacrmoura@gmail.com>
2015-05-27 14:56:56 +0100

    first commit

---
+++ src/projecto/ArvoreCaminhos.java
@@ -0,0 +1,85 @@
+package projecto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ArvoreCaminhos 
+{
+	List<Parede<Coord<Integer, Integer>, Coord<Integer, Integer>>> paredes;
+	List<Coord<Integer, Integer>> caixas;
+	Coord<Integer, Integer> robot;
+
+	public void inserirDados ()
+	{
+		//-.-
+
+	}
+	public void inicializar()
+	{
+		paredes = new ArrayList<Parede<Coord<Integer, Integer>, Coord<Integer, Integer>>>();
+		caixas = new ArrayList<Coord<Integer, Integer>>();
+
+		Coord<Integer, Integer> c1 = new Coord<Integer, Integer>(15,0);
+		Coord<Integer, Integer> c2 = new Coord<Integer, Integer>(15,10);
+		Parede<Coord<Integer, Integer>, Coord<Integer, Integer>> parede = 
+				new Parede<Coord<Integer, Integer>, Coord<Integer, Integer>>(c1,c2);
+		paredes.add(parede);
+		c1 = new Coord<Integer, Integer>(17,5);
+		c2 = new Coord<Integer, Integer>(25,5);
+		parede = new Parede<Coord<Integer, Integer>, Coord<Integer, Integer>>(c1,c2);
+		paredes.add(parede);
+
+		c1 = new Coord<Integer, Integer>(10,0);
+		caixas.add(c1);
+		c1 = new Coord<Integer, Integer>(20,10);
+		caixas.add(c1);
+		c1 = new Coord<Integer, Integer>(20,0);
+		caixas.add(c1);
+
+		robot = new Coord<Integer, Integer>(0,0);	
+	}
+
+	public double calcularDistancia(Coord<Integer, Integer> a, Coord<Integer, Integer> b)
+	{
+		int ax = a.getX();
+		int ay = a.getY();
+		int bx = b.getX();
+		int by = b.getY();
+
+		double dab = Math.sqrt((bx-ax)*(bx-ax)+(by-ay)*(by-ay));
+		return dab;
+
+	}
+
+	public double[] calcularDistancia(Parede<Coord<Integer,Integer>,Coord<Integer,Integer>> a, Coord<Integer, Integer> b)
+	{
+		int v1x = a.getX().getX();
+		int v1y = a.getX().getY();
+		int v2x = a.getY().getX();
+		int v2y = a.getY().getY();
+		int bx = b.getX();
+		int by = b.getY();
+
+		double dav1 = Math.sqrt((bx-v1x)*(bx-v1x)+(by-v1y)*(by-v1y));
+		double dav2 = Math.sqrt((bx-v2x)*(bx-v2x)+(by-v2y)*(by-v2y));
+		double[] ret = new double[2];
+		ret[0] = dav1;
+		ret[1] = dav2;
+		return ret;
+
+	}
+
+	public void intersecao(Coord<Integer,Integer> a,Coord<Integer,Integer> b)
+	{
+		int ax = a.getX();
+		int ay = a.getY();
+		int bx = b.getX();
+		int by = b.getY();
+		 
+		
+	}
+
+
+}
---
+++ src/projecto/Coord.java
@@ -0,0 +1,37 @@
+package projecto;
+
+public class Coord<X,Y> {
+    
+	X x;
+    Y y;
+    
+    public Coord(X x, Y y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public X getX()
+    {
+    	return x;
+    }
+    
+    public Y getY()
+    {
+    	return y;
+    }
+    
+    @Override
+    public boolean equals(Object object)
+    {
+        boolean isEqual= false;
+
+        if (object != null && object instanceof Coord<?,?>)
+        {
+            isEqual = (this.x.equals(((Coord<?,?>) object).x) && (int) this.y == (int) ((Coord<?,?>) object).y);
+        }
+
+        return isEqual;
+    }
+
+}
